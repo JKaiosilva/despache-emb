f2982286926a14a1f4a76412e93ffd0b6dba2b99
Kaio Silva <106553278+JKaiosilva@users.noreply.github.com>
2023-03-01 15:11:51 -0400

    imagens e painel adm

---
+++ config/bancodados.js
@@ -0,0 +1,5 @@
+if(process.env.NODE_ENV == 'production') {
+    module.exports = {mongoURI: ''}
+}else{
+    module.exports = {mongoURI: 'mongodb://0.0.0.0:27017/despache-emb'}
+}
--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,6 @@ const { userInfo } = require('os')
 require('../models/Formulario')
 require('../models/Usuario')
 const Formulario = mongoose.model('formularios')
-const transporter = require('../config/mail')
 const {eUser} = require('../helpers/eUser')
 const moment = require('moment')
 require('../models/Despacho')
@@ -30,70 +29,6 @@ const AvisoSaida = mongoose.model('avisoSaidas')
                 res.redirect('/')
             })
            
-        })
-
-        router.get('/novo', (req, res) => {
-            res.render('formulario/formulario')
-        })
-
-        router.post('/formulario/novo', eUser, (req, res) => {
-            var erros = []
-            if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
-                erros.push({texto: 'Nome inválido!'})
-            }if(!req.body.email || typeof req.body.email == undefined || req.body.email == null){
-                erros.push({texto: 'Email inválido'})
-            }if(!req.body.codigo || typeof req.body.codigo == undefined || req.body.codigo == null){
-                erros.push({texto: 'Código inválido'})
-            }if(!req.body.dsaida || typeof req.body.dsaida == undefined || req.body.dsaida == null){
-                erros.push({texto: 'Data de saída inválida'})
-            }if(!req.body.dvolta || typeof req.body.dvolta == undefined || req.body.dvolta == null){
-                erros.push({texto: 'Data de volta inválida'})
-            }if(req.body.nome < 4){
-                erros.push({texto: 'Nome muito curto'})
-            }if(erros.length > 0){
-                console.log(erros)
-                res.render('/', {erros: erros})
-            }else{
-                
-                const novoFormulario = {
-                    nome: req.body.nome,
-                    codigo: req.body.codigo,
-                    email: req.body.email,
-                    dsaida: req.body.dsaida,
-                    dvolta: req.body.dvolta,
-                    data: moment(Date.now()).format('DD/MM/YYYY HH:mm'),
-                    idUsuario: req.user._id
-                }
-                new Formulario(novoFormulario).save().then(() => {
-                    
-                    req.flash('success_msg', 'Formulário enviado com sucesso')
-                    res.redirect('/formulario/novo')
-                    transporter.sendMail({
-                        from: 'Kaio Silva <crb.app.forms@hotmail.com>',
-                        to: 'kaiofer39@gmail.com' + ',' + req.body.email,
-                        subject: 'teste integrado',
-                        text: 'Segue abaixo as informações inseridas no formulário',
-                        html: 'Nome: ' + req.body.nome + '<br>' + 
-                        'Email: ' + req.body.email + '<br>' +
-                        'codigo: ' + req.body.codigo + '<br>' +
-                        'Data de Saída: ' + req.body.dsaida + '<br>' +
-                        'Data de Volta: ' + req.body.dvolta + '<br>' +
-                        'Data da Solicitação: ' + moment(Date.now()).format('DD/MM/YYYY HH:mm'),
-                    }).then((message) => {
-                        req.flash('success_msg', 'Formulário enviado com sucesso')
-                        console.log(message)
-                    }).catch((err) => {
-                        console.log(err)
-                        req.flash('error_msg', 'Houve um erro ao enviar o formulário')
-                        res.redirect('/formulario/novo')
-                    })
-                }).catch((err) => {
-                    console.log(err)
-                    req.flash('error_msg', 'Houve um erro ao enviar o formulário')
-                    res.redirect('/formulario/novo')
-                })
-
-            }
         })
 
 
