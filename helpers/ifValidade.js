f1ee422fd865b516f4516520a2dfc56adc7af760
Kaio Silva <106553278+JKaiosilva@users.noreply.github.com>
2023-03-28 11:09:52 -0400

    Update ifValidade.js

--- helpers/ifValidade.js
+++ helpers/ifValidade.js
@@ -1,15 +1,33 @@
 const Handlebars = require('handlebars')
 const moment = require('moment')
+const mongoose = require('mongoose')
+require('../models/Despacho')
+const Despacho = mongoose.model('despachos')
 
-Moment = moment(Date.now()).format('DD/MM/YYYY')
 
-var despachos = despachos.despachoDataPedido
 
-module.exports = 
-Handlebars.registerHelper("ifValidade", function(despachos, Moment, options) {
-    if (Moment == despachos) {
+
+
+Moment = moment(Date.now()).format('DD/MM/YYYY') 
+
+
+Despacho.find().lean().then((despachos) => {
+    despachos.forEach((data)=>{
+        console.log(data.despachoDataPedido)
+        return data
+  })
+})
+
+
+Handlebars.registerHelper("ifValidade", function(data, options) {
+    if (data != Moment) {
       return options.fn(this);
     } else {
       return options.inverse(this);
     }
   })
+
+
+
+
+
