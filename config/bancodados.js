360e41fd5587cff039ebc70cda335ba4c0448d52
Kaio Silva <106553278+JKaiosilva@users.noreply.github.com>
2023-05-27 12:47:01 -0400

    DB e tripulantes(acho que finalizei)

--- config/bancodados.js
+++ config/bancodados.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production') {
     module.exports = {mongoURI: 'mongodb+srv://kaiosilva:teste123456@cluster0.tzkgl4r.mongodb.net/?retryWrites=true&w=majority'}
 }else{
-    module.exports = {mongoURI: 'mongodb://0.0.0.0:27017/despache-emb'}
+    module.exports = {mongoURI: 'mongodb://127.0.0.1:27017/despache-emb'}
 }
