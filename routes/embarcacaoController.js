7eff0defb6f70e33ae4769dc4d3bfba21caf290d
Kaio Silva <jkaio37@hotmail.com>
2023-06-20 20:14:27 -0400

    error msg embarcações

--- routes/embarcacaoController.js
+++ routes/embarcacaoController.js
@@ -39,10 +39,10 @@ const bcrypt = require('bcryptjs')
 
 router.get('/formulario/addEmbarcacao', eUser, async (req, res) => {
     try{
-        res.render('formulario/embarcacoes/addEmbarcacao',)
+        res.render('formulario/embarcacoes/addEmbarcacao')
     }catch(err){
         req.flash('error_msg', `Ocorreu um erro ao mostrar formulário de adição de Embarcações (${err})`)
-        res.redirect('/')
+        res.redirect('/formulario')
     }
 })
 
@@ -69,11 +69,11 @@ router.post('/formulario/addEmbarcacao', eUser, (req, res) => {
     }
     new Embarcacao(novaEmbarcacao).save().then(() => {
         req.flash('success_msg', 'Embarcação cadastrada com sucesso, verifique-a na área de formulários')
-        res.redirect('/')
+        res.redirect('/formulario')
     }).catch((err) => {
         console.log(err)
         req.flash('error_msg', `Erro ao salvar Embarcação (${err})`)
-        res.redirect('/')
+        res.redirect('/formulario')
     })
 })
 
@@ -93,15 +93,16 @@ router.get('/formulario/embarcacaoVizu/:id', eUser, async(req, res) => {
         const avisoEntradas = await AvisoEntrada.find({embarcacao: embarcacoes._id}).lean()
         const avisoSaidas = await AvisoSaida.find({embarcacao: embarcacoes._id}).lean()
             res.render('formulario/embarcacoes/embarcacaoVizu',
-                {embarcacoes: embarcacoes,
+                {
+                    embarcacoes: embarcacoes,
                     despachos: despachos,
-                        avisoEntradas: avisoEntradas,
-                            avisoSaidas: avisoSaidas,
-                                hidden: hidden
+                    avisoEntradas: avisoEntradas,
+                    avisoSaidas: avisoSaidas,
+                    hidden: hidden
                 })
     }catch(err){
-        req.flash('error_msg', 'Erro ao mostrar Embarcação')
-        res.redirect('/')
+        req.flash('error_msg', `Erro ao visualizar esta Embarcação (${err})`)
+        res.redirect('/formulario')
     }
 })
 
@@ -113,10 +114,11 @@ router.get('/embarcacoes', eUser, async (req, res) => {
     try{
         const embarcacoes = await Embarcacao.find({usuarioID: req.user._id}).limit(5).lean().sort({embarcacaoNome: 'asc'})
             res.render('formulario/embarcacoes/embarcacoes', 
-                {embarcacoes: embarcacoes
-            })
+                {
+                    embarcacoes: embarcacoes
+                })
     }catch(err){
-        req.flash('error_msg', 'Erro ao mostrar página')
+        req.flash('error_msg', `Erro ao listar Embarcações (${err})`)
         res.redirect('/formulario')
     }
 })
@@ -146,13 +148,14 @@ router.get('/embarcacoes/:page', eUser, async (req, res) => {
         }
         const embarcacoes = await Embarcacao.find({usuarioID: req.user._id}).skip(skip).limit(limit).lean().sort({embarcacaoNome: 'asc'})
             res.render('formulario/embarcacoes/embarcacoesPage', 
-                {embarcacoes: embarcacoes,
+                {
+                    embarcacoes: embarcacoes,
                     nextPage: nextPage,
-                        previousPage: previousPage,
-                            hidden: hidden
+                    previousPage: previousPage,
+                    hidden: hidden
                 })
     }catch(err){
-        req.flash('error_msg', 'Erro ao mostrar página')
+        req.flash('error_msg', `Erro ao paginar Embarcações (${err})`)
         res.redirect('/formulario')
      }
 })
@@ -164,11 +167,13 @@ router.get('/embarcacoes/:page', eUser, async (req, res) => {
 router.get('/admin/embarcacoesEdit/:id', Admin, async(req, res) => {
     try{
         const embarcacao = await Embarcacao.findOne({_id: req.params.id}).lean()
-        res.render('admin/embarcacoes/embarcacaoEdit', {
-            embarcacao: embarcacao
-        })
+            res.render('admin/embarcacoes/embarcacaoEdit', 
+                {
+                    embarcacao: embarcacao
+                })
     }catch(err){
-
+        req.flash('error_msg', `Erro ao mostar página de edição de Embarcação (${err})`)
+        res.redirect('/admin/painel')
     }
 })
 
@@ -193,11 +198,11 @@ router.post('/admin/embarcacoesEdit', Admin, async(req, res) => {
             embarcacaoValidadeNumber: Date.parse(req.body.embarcacaoValidadeNumber)
         })
         req.flash('success_msg', 'Embarcação editada com sucesso!')
-        res.redirect('painel')
+        res.redirect('/admin/painel')
     }catch(err){
         console.log(err)
-        req.flash('error_msg', 'Erro ao editar embarcação.')
-        res.redirect('painel')
+        req.flash('error_msg', `Erro ao editar Embarcação (${err})`)
+        res.redirect('/admin/painel')
     }
 })
 
@@ -205,13 +210,17 @@ router.post('/admin/embarcacoesEdit', Admin, async(req, res) => {
 //----  Rota de listagem de Embarcação(admin)    ----//
 
 
-router.get('/admin/embarcacoes', Admin, (req, res) => {
-    Embarcacao.find().limit(5).lean().sort({ EmbarcacaoNome: 'asc' }).then((embarcacoes) => {
-        res.render('admin/embarcacoes/listaEmbarcacoes', { embarcacoes: embarcacoes })
-    }).catch((err) => {
-        req.flash('error_msg', 'Erro interno ao mostrar embarcações')
-        res.redirect('/')
-    })
+router.get('/admin/embarcacoes', Admin, async (req, res) => {
+    try{
+        const embarcacoes = await Embarcacao.find().limit(5).lean().sort({ EmbarcacaoNome: 'asc' })
+            res.render('admin/embarcacoes/listaEmbarcacoes', 
+                { 
+                    embarcacoes: embarcacoes 
+                })
+    } catch(err){
+        req.flash('error_msg', `Erro ao listar Embarcações (${err})`)
+        res.redirect('/admin/painel')
+    }
 })
 
 
@@ -246,7 +255,8 @@ router.get('/admin/embarcacoes/:page', Admin, async (req, res) => {
                 hidden: hidden
             })
     } catch (err) {
-
+        req.flash('error_msg', `Erro ao paginar Embarcações (${err})`)
+        res.redirect('/admin/painel')
     }
 })
 
@@ -254,61 +264,65 @@ router.get('/admin/embarcacoes/:page', Admin, async (req, res) => {
 //----    Rota de formulação de PDF da Embarcação   ----//
 
 
-router.get('/embarcacao/:id/pdf', Admin, (req, res) => {
-    Embarcacao.findById(req.params.id).lean().then((embarcacoes) => {
-      const html = `
-      <form>
-      <input  type="hidden" name="usuarioID" value="%= req.user._id %">
-      <div>
-          <h4 class="text-center">Dados da Embarcação</h4>
-              <div>
-                  <label>Nome da Embarcação</label>
-                  <input value="${embarcacoes.embarcacaoNome}" name="embarcacaoNome" type="text" disabled>
-                  <label>Tipo da Embarcação</label>
-                  <input value="${embarcacoes.embarcacaoTipo}" name="embarcacaoTipo" type="text" disabled>
-              </div>
-      </div>
-      <div>
-          <label>Bandeira</label>
-          <input value="${embarcacoes.embarcacaoBandeira}" name="embarcacaoBandeira" type="text" disabled>
-          <label>N° Inscrição na Autoridade Marítima do Brasil</label>
-          <input value="${embarcacoes.embarcacaoNInscricaoautoridadeMB}" name="embarcacaoNInscricaoautoridadeMB" type="text" disabled>
-      </div>
-      <div>
-          <label>Arqueação Bruta</label>
-          <input value="${embarcacoes.embarcacaoArqueacaoBruta}" name="embarcacaoArqueacaoBruta" type="text" disabled>
-          <label>Comprimento Total</label>
-          <input value="${embarcacoes.embarcacaoComprimentoTotal}" name="embarcacaoComprimentoTotal" type="text" disabled>
-          <label>Tonelagem Porte Bruto</label>
-          <input value="${embarcacoes.embarcacaoTonelagemPorteBruto}" name="embarcacaoTonelagemPorteBruto" type="text" disabled>
-      </div>
-      <div>
-          <label>Certificado de Registro do Amador(CRA)</label>
-          <input value="${embarcacoes.embarcacaoCertificadoRegistroAmador}" name="embarcacaoCertificadoRegistroAmador" type="text" disabled>
-          <label>Armador</label>
-          <input value="${embarcacoes.embarcacaoArmador}" name="embarcacaoArmador" type="text" disabled>
-      </div>
-              <div>
-                  <label>N° do CRA</label>
-                  <input value="${embarcacoes.embarcacaoNCRA}" name="embarcacaoNCRA" type="text" disabled>
-                  <label>Validade</label>
-                  <input value="${embarcacoes.embarcacaoValidade}" name="embarcacaoValidade" type="text" disabled>
-              </div>
-      </div>
-  </form>
-</div>
-</div>
-
-      `;
-
-        pdf.create(html).toStream((err, stream) => {
-        if (err) return res.send(err);
-        res.attachment(`${embarcacoes.embarcacaoNome}.pdf`);
-        res.setHeader('Content-Type', 'application/pdf');
-        stream.pipe(res);
-      });
-  });
-
+router.get('/embarcacao/:id/pdf', Admin, async (req, res) => {
+    try{
+        const embarcacoes = await Embarcacao.findById(req.params.id).lean()
+        const html = `
+        <form>
+        <input  type="hidden" name="usuarioID" value="%= req.user._id %">
+        <div>
+            <h4 class="text-center">Dados da Embarcação</h4>
+                <div>
+                    <label>Nome da Embarcação</label>
+                    <input value="${embarcacoes.embarcacaoNome}" name="embarcacaoNome" type="text" disabled>
+                    <label>Tipo da Embarcação</label>
+                    <input value="${embarcacoes.embarcacaoTipo}" name="embarcacaoTipo" type="text" disabled>
+                </div>
+        </div>
+        <div>
+            <label>Bandeira</label>
+            <input value="${embarcacoes.embarcacaoBandeira}" name="embarcacaoBandeira" type="text" disabled>
+            <label>N° Inscrição na Autoridade Marítima do Brasil</label>
+            <input value="${embarcacoes.embarcacaoNInscricaoautoridadeMB}" name="embarcacaoNInscricaoautoridadeMB" type="text" disabled>
+        </div>
+        <div>
+            <label>Arqueação Bruta</label>
+            <input value="${embarcacoes.embarcacaoArqueacaoBruta}" name="embarcacaoArqueacaoBruta" type="text" disabled>
+            <label>Comprimento Total</label>
+            <input value="${embarcacoes.embarcacaoComprimentoTotal}" name="embarcacaoComprimentoTotal" type="text" disabled>
+            <label>Tonelagem Porte Bruto</label>
+            <input value="${embarcacoes.embarcacaoTonelagemPorteBruto}" name="embarcacaoTonelagemPorteBruto" type="text" disabled>
+        </div>
+        <div>
+            <label>Certificado de Registro do Amador(CRA)</label>
+            <input value="${embarcacoes.embarcacaoCertificadoRegistroAmador}" name="embarcacaoCertificadoRegistroAmador" type="text" disabled>
+            <label>Armador</label>
+            <input value="${embarcacoes.embarcacaoArmador}" name="embarcacaoArmador" type="text" disabled>
+        </div>
+                <div>
+                    <label>N° do CRA</label>
+                    <input value="${embarcacoes.embarcacaoNCRA}" name="embarcacaoNCRA" type="text" disabled>
+                    <label>Validade</label>
+                    <input value="${embarcacoes.embarcacaoValidade}" name="embarcacaoValidade" type="text" disabled>
+                </div>
+            </div>
+        </form>
+    </div>
+    </div>
+  
+        `;
+  
+          pdf.create(html).toStream((err, stream) => {
+          if (err) return res.send(err);
+          res.attachment(`${embarcacoes.embarcacaoNome}.pdf`);
+          res.setHeader('Content-Type', 'application/pdf');
+          stream.pipe(res);
+        });
+    ;
+    }catch(err){
+        req.flash('error_msg', `Erro ao gerar PDF desta Embarcação (${err})`)
+        res.redirect('/admin/painel')
+    }
 })
 
 
